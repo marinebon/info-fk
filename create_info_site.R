4e257d3ad28ce9a2667744fc9b8d7b46f3c2353c
7yl4r <murray.tylar@gmail.com>
2018-11-07 16:29:07 -0500

    attempt to make travis work for building infographic

--- create_info_site.R
+++ create_info_site.R
@@ -4,7 +4,7 @@
 # # Rscript create_info_site.R
 
 # this script assumes you have already installed the infographiq package
-# devtools::install_github('marinebon/infographiq')
+devtools::install_github('marinebon/infographiq')
 infographiq::create_info_site(
     render_modals = T
 )
