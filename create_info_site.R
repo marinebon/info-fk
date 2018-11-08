d3ffe8ddb6ea2d300d6bebba783bb3f8455664f1
7yl4r <murray.tylar@gmail.com>
2018-11-08 12:18:41 -0500

    fix build & commit iff build success

--- create_info_site.R
+++ create_info_site.R
@@ -1,10 +1,36 @@
 #!/usr/bin/env Rscript
 #
 # run w/ Rscript like:
-# # Rscript create_info_site.R
+# Rscript create_info_site.R
 
-# this script assumes you have already installed the infographiq package
+REPOS="https://cran.us.r-project.org"
+# =========================================================================
+# === This script assumes you have already installed the infographiq package.
+# === Here is the install process:
+# === Install the infographiq R package and those it depends on.
+# === You only need to do this once.
+# =========================================================================
+# install required packages
+
+# 1) devtools
+if (!require('devtools')) install.packages('devtools', repos="https://cran.us.r-project.org")
+
+# 2) stuff from DESCRIPTION:
+#    manually:
+# install.packages(c("tidyverse", "stringr", "rmarkdown", "dygraphs", "xts", "lubridate", "geojsonio", "RColorBrewer", "leaflet", "crosstalk", "servr", "roxygen2", "futile.logger"))
+#    using DESCRIPTION:
+devtools::install("./", dependencies=TRUE, repos="https://cloud.r-project.org/")
+
+# install infographiq from Github
 devtools::install_github('marinebon/infographiq')
+
+
+# =========================================================================
+# === Load the library and create the site.
+# =========================================================================
+# load
+library(infographiq)
+
 infographiq::create_info_site(
     render_modals = T
 )
